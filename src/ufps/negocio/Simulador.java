216027311c0568ff9646cae89a1bc7fe90f86178
Marco Antonio Adarme Jaimes <madarme@ufps.edu.co>
2022-09-13 06:15:53 -0500

    Plantilla

---
+++ src/ufps/negocio/Simulador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufps.negocio;
+
+import ufps.modelo.Proceso;
+
+/**
+ *
+ * @author madarme
+ */
+public class Simulador {
+ 
+    private Proceso cadena_procesos[];
+           
+}
---
+++ src/ufps/vistas/Test_archivo_http.java
@@ -0,0 +1,26 @@
+package ufps.vistas;
+import ufps.util.varios.*;
+
+/**
+ * Write a description of class Test_archivo_http here.
+ * 
+ * @author marco adarme
+ * @version (a version number or a date)
+ */
+public class Test_archivo_http
+{
+   
+    public static void main(String args[])
+    {
+        String url="https://madarme.co/persistencia/process.txt";
+        ArchivoLeerURL file=new ArchivoLeerURL(url);
+        Object v[]=file.leerArchivo();
+        short i=0;
+        for(Object datos:v)
+            System.out.println("Línea "+(i++)+":"+datos.toString());
+            
+    }
+    
+
+    
+}
