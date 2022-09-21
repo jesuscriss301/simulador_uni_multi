f6e395540e4ca3ac177bf797a8f5ab593701c6a9
Jesus Cristancho <jesusmanuelcp@ufps.edu.co>
2022-09-21 08:47:34 -0500

    validacion entrada de procesos

--- src/ufps/modelo/Proceso.java
+++ src/ufps/modelo/Proceso.java
@@ -1,40 +1,71 @@
 /*
+
  * To change this license header, choose License Headers in Project Properties.
+
  * To change this template file, choose Tools | Templates
+
  * and open the template in the editor.
+
  */
+
 package ufps.modelo;
 
+
 /**
+
  *
+
  * @author madarme
+
  */
+
 public class Proceso {
+
     //P0... Pn
+
     private int id_proceso;
+
     //RRIIRR...
+
     private String cadena_ejecucion;
 
+
     public Proceso() {
+
     	this.id_proceso=0;
+
     	this.cadena_ejecucion="";
+
     }
+
 
     public int getId_proceso() {
+
         return id_proceso;
+
     }
+
 
     public void setId_proceso(int id_proceso) {
+
         this.id_proceso = id_proceso;
+
     }
+
 
     public String getCadena_ejecucion() {
+
         return cadena_ejecucion;
+
     }
 
+
     public void setCadena_ejecucion(String cadena_ejecucion) {
+    	if (cadena_ejecucion.charAt(0)=='R'&& cadena_ejecucion.charAt(-1)=='R') {
         this.cadena_ejecucion = cadena_ejecucion;
+    	}
     }
+
     
     
 }
